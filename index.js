1bc101655ef0501a1e31d4a3ff28f60077a759cb
bdabin <bdb0731@gmail.com>
2024-06-24 17:00:41 +0900

    add(cors): CORS 패키지 추가

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 3000;
 const { Sequelize } = require("sequelize");
 const db = require("./models");
 const swaggerDocs = require("./swagger");
+const cors = require("cors");
 
 const userRouter = require("./routes/user");
 const wordRouter = require("./routes/word");
 const whatsappRouter = require("./routes/whatsapp");
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3001" }));
 
 app.use("/users", userRouter);
 app.use("/words", wordRouter);
