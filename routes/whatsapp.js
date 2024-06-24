0752403f6d358dc1f92e13eca2e5b9b763a363fb
bdabin <bdb0731@gmail.com>
2024-06-24 17:43:46 +0900

    fix(whatsapp): 왓츠앱 api 테스트 완료

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,37 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const axios = require("axios").create({
+  baseURL: "https://graph.facebook.com/v19.0/110677915346811",
+});
 
+/**
+ * @swagger
+ * /whatsapp/send-message:
+ *   post:
+ *     summary: WhatsApp 메시지 발송
+ *     description: 특정 전화번호로 WhatsApp 메시지를 발송합니다.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *                 description: 사용자 ID
+ *     responses:
+ *       '200':
+ *         description: WhatsApp 메시지 발송 성공
+ *       '404':
+ *         description: 사용자를 찾을 수 없음
+ *       '500':
+ *         description: 서버 오류
+ */
 router.post("/send-message", async (req, res) => {
   try {
-    const userId = req.body.userId;
-    const user = await db.User.findByPk(userId);
+    // const userId = req.body.userId;
+    // const user = await db.User.findByPk(userId);
 
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
+    // if (!user) {
+    //   return res.status(404).json({ error: "User not found" });
+    // }
 
-    const phoneNumber = user.phoneNumber;
-
+    const phoneNumber = "821020252266";
     // WhatsApp 메시지 발송 API 호출
     const response = await axios.post(
-      "https://graph.facebook.com/v19.0/176451042228268/messages",
+      "/messages",
+      {
+        messaging_product: "whatsapp",
+        to: phoneNumber,
+        type: "template",
+        template: {
+          name: "hello_world",
+          language: {
+            code: "en_US",
+          },
+        },
+      },
       {
         headers: {
           Authorization:
-            "Bearer EABw1aapKBTcBO4HmhFlWUyXbWRHcY3NCRzTbnMGy4Fapb17dsddbPAyMvMAvoBKcGU0k2cuXmEvXmCXvvQP5u4fD4oHz0illcdJZCv3Aa9hlGTdOBqrn1LBfV5ZCU3OrgMVi0p9UPku3qBrl7hPZCcr2ZA5GTx5HGpu7ExsnHrll5oyWAP6mmkgEpVKdSD2iTbF0yat0IBYshIoG6OpwtBscHTYZD",
+            "Bearer EABw1aapKBTcBOzKB0WGiFzBnSSssOXZCrKFKqPchVWAbEkgcH6xBfZCaDypQgUxmBVKr4ZBWJfwVz8MZBeSeZArC3jITrbDkg65SFcKFwYWf4ZB8IWtMweEfmMckEdUiTWlKZCQBd7ZC2Q4NJwrBdoX0N0lxqyADc9eEJTHeBgBLr8T0tHF34CdBWlDQyc9VelfuKxkEJsnn44dGZCzKr3QZAoKOm2WxZCS",
           "Content-Type": "application/json",
-        },
-        data: {
-          messaging_product: "whatsapp",
-          to: phoneNumber,
-          type: "template",
-          template: {
-            name: "hello_world",
-            language: {
-              code: "en_US",
-            },
-          },
         },
       }
     );
@@ -43,6 +69,7 @@ router.post("/send-message", async (req, res) => {
       response: data,
     });
   } catch (error) {
+    console.log(error.response.data);
     res.status(500).json({ error: "Something went wrong" });
   }
 });
