01815cc2032a5c2d7d5d9151466a50cdea444c96
bdabin <bdb0731@gmail.com>
2024-06-29 10:33:20 +0900

    fix(whatsapp): 왓츠앱 발송용 코드

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -5,12 +5,86 @@ const axios = require("axios").create({
   baseURL: "https://graph.facebook.com/v17.0/110677915346811",
 });
 
+const sendDailyConversation = async (phoneNumber) => {
+  const data = await db.Word.findOne({
+    where: { id: 1 },
+  });
+
+  if (!data) {
+    throw new Error("Word not found");
+  }
+
+  const components = [
+    {
+      type: "header",
+      parameters: [
+        {
+          type: "text",
+          text: data.korean,
+        },
+      ],
+    },
+    {
+      type: "body",
+      parameters: [
+        {
+          type: "text",
+          text: data.pronunciation,
+        },
+        {
+          type: "text",
+          text: data.description,
+        },
+        {
+          type: "text",
+          text: data.example_1,
+        },
+        {
+          type: "text",
+          text: data.example_2,
+        },
+        {
+          type: "text",
+          text: data.example_3,
+        },
+      ],
+    },
+  ];
+
+  const response = await axios.post(
+    "/messages",
+    {
+      messaging_product: "whatsapp",
+      to: phoneNumber,
+      type: "template",
+      template: {
+        name: process.env.TEMPLATE_WELCOME,
+        language: {
+          code: "id_ID",
+        },
+        components,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response;
+};
+
 /**
  * @swagger
  * /whatsapp/send-message:
  *   post:
  *     summary: WhatsApp 메시지 발송
  *     description: 특정 전화번호로 WhatsApp 메시지를 발송합니다.
+ *     tags:
+ *       - WhatsApp
+ *     operationId: sendWhatsAppMessage
  *     requestBody:
  *       required: true
  *       content:
@@ -22,72 +96,115 @@ const axios = require("axios").create({
  *                 type: string
  *                 description: 사용자 ID
  *     responses:
- *       '200':
+ *       200:
  *         description: WhatsApp 메시지 발송 성공
- *       '404':
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "WhatsApp 메시지가 성공적으로 발송되었습니다."
+ *                 response:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       status:
+ *                         type: string
+ *                         example: "sent"
+ *       404:
  *         description: 사용자를 찾을 수 없음
- *       '500':
+ *       500:
  *         description: 서버 오류
  */
 router.post("/send-message", async (req, res) => {
   try {
-    // const userId = req.body.userId;
-    // const user = await db.User.findByPk(userId);
+    const activeUsers = await db.User.findAll({
+      where: { status: "active" },
+    });
+    const result = [];
 
-    // if (!user) {
-    //   return res.status(404).json({ error: "User not found" });
-    // }
-
-    const phoneNumber = "821020252266";
-    // WhatsApp 메시지 발송 API 호출
-    const response = await axios.post(
-      "/messages",
-
-      {
-        // "messaging_product": "whatsapp",
-        // "recipient_type": "individual",
-        // "to": "<WHATSAPP_USER_PHONE_NUMBER>",
-        // "type": "text",
-        // "text": {
-        //   "preview_url": <ENABLE_LINK_PREVIEW>,
-        //   "body": "<BODY_TEXT>"
-        // }
-
-        messaging_product: "whatsapp",
-        preview_url: false,
-        recipient_type: "individual",
-        to: phoneNumber,
-        type: "text",
-        text: {
-          body: "Hello, My name is brooks.",
-        },
-        language: { code: "en_US" },
-      },
-      // {
-      //   messaging_product: "whatsapp",
-      //   to: phoneNumber,
-      //   type: "template",
-      //   template: {
-      //     name: "hello_world",
-      //     language: {
-      //       code: "en_US",
-      //     },
-      //   },
-      // },
-      {
-        headers: {
-          Authorization:
-            "Bearer EABw1aapKBTcBO0LxjPNLuKah3c6PnQvUJLZAjLocZB8Ri9ogFuDaISK1Glgz7rhyfZBLTrnnbuitoR628F6paeD1NUGZCjjTRt34TOvQ1QKj8LlmQ1BBJJPAhIzTYcd19zNZAeZCyvdtRaTw7JhtTvFpbetywE8ZAu5iPPP3FHGiguLRuWWeDjxF0Pj0WKTddzKywEfpEefAfqJ8z3ZAJZA2wGmKnVeQZD",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const data = response.data;
+    for (const user of activeUsers) {
+      const phoneNumber = user.phoneNumber; // 가정: User 모델에 phoneNumber 필드가 있다고 가정합니다.
+      // WhatsApp 메시지 발송 API 호출
+      const response = await sendDailyConversation(phoneNumber);
+      result.push(response.data);
+    }
 
     res.json({
       message: "WhatsApp 메시지가 성공적으로 발송되었습니다.",
-      response: data,
+      response: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+/**
+ * @swagger
+ * /welcome:
+ *   post:
+ *     summary: 활성 사용자에게 환영 메시지를 WhatsApp으로 발송
+ *     description: 활성 상태인 모든 사용자에게 WhatsApp을 통해 환영 메시지를 보냅니다.
+ *     responses:
+ *       200:
+ *         description: 메시지가 성공적으로 발송되었습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "WhatsApp 메시지가 성공적으로 발송되었습니다."
+ *                 response:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       status:
+ *                         type: string
+ *                         example: "sent"
+ *       500:
+ *         description: 서버 오류
+ */
+router.post("/welcome", async (req, res) => {
+  try {
+    const activeUsers = await db.User.findAll({
+      where: { status: "active" },
+    });
+
+    const result = [];
+    for (const user of activeUsers) {
+      const phoneNumber = user.phoneNumber; // 가정: User 모델에 phoneNumber 필드가 있다고 가정합니다.
+      // WhatsApp 메시지 발송 API 호출
+      const response = await axios.post(
+        "/messages",
+        {
+          messaging_product: "whatsapp",
+          to: phoneNumber,
+          type: "template",
+          template: {
+            name: process.env.TEMPLATE_WELCOME,
+            language: {
+              code: "id_ID",
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      result.push(response.data);
+    }
+
+    res.json({
+      message: "WhatsApp 메시지가 성공적으로 발송되었습니다.",
+      response: result,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
