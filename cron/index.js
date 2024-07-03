84c7a13c3a3af23002f2a0c1a422c6c7f0bf4212
bdabin <bdb0731@gmail.com>
2024-07-03 16:13:37 +0900

    fix(cron): cron date 테스트

--- cron/index.js
+++ cron/index.js
@@ -4,17 +4,35 @@ const axios = require("axios");
 // 서버 시간이 몇시 기준으로 돼있는지 테스트하는 코드
 cron.schedule("* * * * *", () => {
   const now = new Date();
+  const sendAt = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    11,
+    0,
+    0,
+    0
+  ); // 한국 시간 날짜로 그 날짜의 오전 11시
+  console.log(`API 전달값: ${sendAt.toISOString()}`);
   console.log(`현재 서버 시간: ${now.toISOString()}`);
 });
 
-// 매일 한국 시간 오후 4시 5분에 작동하는 cron 작업을 설정합니다.
-// 한국 시간은 UTC+9이므로, UTC 시간으로는 오전 7시 5분입니다.
-cron.schedule("5 7 * * *", async () => {
+// 매일 한국 시간 오전 0시 5분에 작동하는 cron 작업을 설정합니다.
+// 한국 시간은 UTC+9이므로, UTC 시간으로는 오후 3시 5분입니다.
+cron.schedule("5 15 * * *", async () => {
   try {
     console.log("매일 오전 0시 5분에 작동하는 작업 시작");
 
     const now = new Date();
-    const sendAt = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 15, 5, 0, 0)); // 한국 시간 0시 5분은 UTC로 당일 15시 5분
+    const sendAt = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      11,
+      0,
+      0,
+      0
+    ); // 한국 시간 날짜로 그 날짜의 오전 11시
     console.log(`API 전달값: ${sendAt.toISOString()}`);
     // 예를 들어, 특정 API를 호출하는 경우:
     // const response = await axios.get("https://example.com/api/daily-task", { params: { sendAt } });
