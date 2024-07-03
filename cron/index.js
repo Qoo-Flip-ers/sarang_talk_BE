1badffe5bcb7f628b35fd835c797f313829a4eb9
bdabin <bdb0731@gmail.com>
2024-07-03 16:10:39 +0900

    fix(cron): cron utc 계산식 추가

--- cron/index.js
+++ cron/index.js
@@ -7,15 +7,17 @@ cron.schedule("* * * * *", () => {
   console.log(`현재 서버 시간: ${now.toISOString()}`);
 });
 
-// 매일 한국 시간 오후 4시 2분에 작동하는 cron 작업을 설정합니다.
-// 한국 시간은 UTC+9이므로, UTC 시간으로는 오전 7시 2분입니다.
+// 매일 한국 시간 오후 4시 5분에 작동하는 cron 작업을 설정합니다.
+// 한국 시간은 UTC+9이므로, UTC 시간으로는 오전 7시 5분입니다.
 cron.schedule("5 7 * * *", async () => {
   try {
-    console.log("매일 오후 4시 2분에 작동하는 작업 시작");
+    console.log("매일 오전 0시 5분에 작동하는 작업 시작");
 
-    // 여기에 실행할 로직을 추가합니다.
+    const now = new Date();
+    const sendAt = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 15, 5, 0, 0)); // 한국 시간 0시 5분은 UTC로 당일 15시 5분
+    console.log(`API 전달값: ${sendAt.toISOString()}`);
     // 예를 들어, 특정 API를 호출하는 경우:
-    // const response = await axios.get("https://example.com/api/daily-task");
+    // const response = await axios.get("https://example.com/api/daily-task", { params: { sendAt } });
     // console.log("API 호출 결과:", response.data);
 
     console.log("작업이 성공적으로 완료되었습니다.");
