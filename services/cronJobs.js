20e43c4fc1e11c82c00fb867bbe6adfad73627cb
bdabin <bdb0731@gmail.com>
2024-07-08 17:37:41 +0900

    fix(cron): cron redis 체크용 로직에 로그 찍기

--- services/cronJobs.js
+++ services/cronJobs.js
@@ -3,6 +3,7 @@ const { processQueue } = require("./queueProcessor");
 
 // 매 분마다 큐를 확인하여 처리
 cron.schedule("* * * * *", () => {
+  console.log("Running cron job");
   if (process.env.NODE_ENV === "production") {
     processQueue();
   }
