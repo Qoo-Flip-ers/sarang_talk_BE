780584485bdc6f2de8dea033df4b8486fc629484
bdabin <bdb0731@gmail.com>
2024-07-11 23:14:15 +0900

    -m

--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,14 @@ router.post("/login", async (req, res) => {
   return res.status(401).json({ error: "Invalid credentials" });
 });
 
+router.post("/refresh", async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (refreshToken === process.env.AUTH_TOKEN) {
+    return res.status(200).json({ token: process.env.AUTH_TOKEN });
+  }
+
+  return res.status(400).json({ error: "Invalid credentials" });
+});
+
 module.exports = router;
