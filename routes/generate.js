d3a7dd6b2c7ab36ecfbcee1cd7e4544373c6817e
bdabin <bdb0731@gmail.com>
2024-09-21 21:03:58 +0900

    fix(puppeteer): headless 에다가 sandbox 값 추가

--- routes/generate.js
+++ routes/generate.js
@@ -137,8 +137,10 @@ router.get("/pronunciation", async (req, res) => {
     const url = `https://ko.dict.naver.com/#/search?query=${encodeURIComponent(
       word
     )}`;
-
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
