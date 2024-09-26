151a55e5b5413e32274ea2633ef2f1b745214a02
bdabin <bdb0731@gmail.com>
2024-09-26 16:33:40 +0900

    fix(test): template 발송 로직 다시 주석제거

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -281,34 +281,35 @@ router.post("/daily", async (req, res) => {
   const to = `whatsapp:${phoneNumber}`;
 
   try {
-    // const response = await client.messages.create({
-    //   from: process.env.FROM_PHONE_NUMBER,
-    //   to,
-    //   contentSid:
-    //     lang === "EN"
-    //       ? process.env.TEMPLATE_EN_DAILY_CONVERSATION
-    //       : process.env.TEMPLATE_DAILY_CONVERSATION,
-    //   contentVariables: JSON.stringify({
-    //     1: todayWord.korean?.trim(),
-    //     2: todayWord.pronunciation?.trim(),
-    //     3:
-    //       lang === "EN"
-    //         ? todayWord.en_description?.trim()
-    //         : todayWord.description?.trim(),
-    //     4: todayWord.example_1?.trim(),
-    //     5: todayWord.example_2?.trim(),
-    //     6:
-    //       lang === "EN"
-    //         ? todayWord.en_example_3?.trim()
-    //         : todayWord.example_3?.trim(),
-    //   }),
-    // });
+    const response = await client.messages.create({
+      from: process.env.FROM_PHONE_NUMBER,
+      messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
+      to,
+      contentSid:
+        lang === "EN"
+          ? process.env.TEMPLATE_EN_DAILY_CONVERSATION
+          : process.env.TEMPLATE_DAILY_CONVERSATION,
+      contentVariables: JSON.stringify({
+        1: todayWord.korean?.trim(),
+        2: todayWord.pronunciation?.trim(),
+        3:
+          lang === "EN"
+            ? todayWord.en_description?.trim()
+            : todayWord.description?.trim(),
+        4: todayWord.example_1?.trim(),
+        5: todayWord.example_2?.trim(),
+        6:
+          lang === "EN"
+            ? todayWord.en_example_3?.trim()
+            : todayWord.example_3?.trim(),
+      }),
+    });
 
     console.log("예약된 메시지가 다음 사용자에게 전송되었습니다:", phoneNumber);
     if (todayWord.imageUrl) {
       setTimeout(async () => {
         await client.messages.create({
-          // from: process.env.FROM_PHONE_NUMBER,
+          from: process.env.FROM_PHONE_NUMBER,
           messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
           to,
           mediaUrl:
@@ -321,7 +322,7 @@ router.post("/daily", async (req, res) => {
     if (todayWord.audioUrl) {
       setTimeout(async () => {
         await client.messages.create({
-          // from: process.env.FROM_PHONE_NUMBER,
+          from: process.env.FROM_PHONE_NUMBER,
           messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
           to,
           mediaUrl: [
