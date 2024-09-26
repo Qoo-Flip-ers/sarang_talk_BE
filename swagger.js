fd793b7d70c795a2a30d32b80d8420b16229a59c
bdabin <bdb0731@gmail.com>
2024-09-26 15:20:04 +0900

    docs(swagger): swagger url 변경

--- swagger.js
+++ swagger.js
@@ -22,13 +22,15 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app, port) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.get("/api-docs.json", (req, res) => {
+  app.use("/api-docs-whatsapp", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/api-docs-whatsapp.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
 
-  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
+  console.log(
+    `Swagger docs available at http://localhost:${port}/api-docs-whatsapp`
+  );
 }
 
 module.exports = swaggerDocs;
