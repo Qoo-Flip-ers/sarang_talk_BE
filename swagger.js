7442c098c5d757bb1a5c2ed28bfa8a7b7f3f2df2
bdabin <bdb0731@gmail.com>
2024-09-26 15:05:52 +0900

    docs(swagger): swagger version 변경

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "Express API with Swagger",
-      version: "1.0.0",
+      version: "1.0.1",
       description:
         "A simple CRUD API application with Express and documented with Swagger",
     },
