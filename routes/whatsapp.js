cdae22fbc1126075862d362c80577a8b05a33b18
bdabin <bdb0731@gmail.com>
2024-09-26 16:56:52 +0900

    fix(test): .png 로 템플릿 이미지 테스트

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -324,10 +324,7 @@ router.post("/daily", async (req, res) => {
           lang === "EN"
             ? todayWord.en_example_3?.trim()
             : todayWord.example_3?.trim(),
-        7: todayWord.imageUrl.replace(
-          "https://annyeongwa.blob.core.windows.net/",
-          ""
-        ),
+        7: "images/Frame_6929.png",
       }),
     });
     console.log("이미지 메시지가 5초 후에 전송되었습니다");
