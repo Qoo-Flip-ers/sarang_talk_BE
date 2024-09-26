a20946793734a3e804c0197a3f2802359a5588ca
bdabin <bdb0731@gmail.com>
2024-09-26 16:54:09 +0900

    fix(test): template url 수정

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -324,7 +324,10 @@ router.post("/daily", async (req, res) => {
           lang === "EN"
             ? todayWord.en_example_3?.trim()
             : todayWord.example_3?.trim(),
-        7: todayWord.imageUrl,
+        7: todayWord.imageUrl.replace(
+          "https://annyeongwa.blob.core.windows.net/",
+          ""
+        ),
       }),
     });
     console.log("이미지 메시지가 5초 후에 전송되었습니다");
