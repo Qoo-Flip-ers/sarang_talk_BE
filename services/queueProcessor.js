0fcf60238ae6c04d80c4018735f620b09a5ef459
Vinzh <kevin.zhong@gmail.com>
2024-10-30 20:03:24 +0700

    create field correct

--- services/queueProcessor.js
+++ services/queueProcessor.js
@@ -80,10 +80,10 @@ async function insertIntoDatabase(data) {
         plan,
         quiz: newQuiz,
         zoom: newZoom,
-        AddOn1Zoom1n1,
-        AddOn2Zoom1n5,
-        AddOn3lecture,
-        AddOn4recap
+        zoom1n1: AddOn1Zoom1n1,
+        zoom1n5: AddOn2Zoom1n5,
+        lectureVideo: AddOn3lecture,
+        liveStudy: AddOn4recap
       });
     });
 
